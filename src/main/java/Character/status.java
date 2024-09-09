77596e5299395debfc70fbddee12f0e26355f045
Eder de Bastos <eder.bastos21@gmail.com>
2024-09-09 17:13:22 -0300

    add character status class and package

---
+++ src/main/java/Character/status.java
@@ -0,0 +1,22 @@
+package Character;
+
+public class status {
+
+    String name;
+    int health;
+    int strength;
+    int dexterity;
+    int constitution;
+    int intelligence;
+    int charisma;
+
+    public status (String name, int health, int strength, int dexterity, int constitution, int intelligence, int charisma){
+        name = this.name;
+        health = this.health;
+        strength = this.strength;
+        dexterity = this.dexterity;
+        constitution = this.constitution;
+        intelligence = this.intelligence;
+        charisma = this.charisma;
+    }
+}
