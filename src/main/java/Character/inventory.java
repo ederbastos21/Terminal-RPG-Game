c6d35c427c525d3e7e01ed8a861bc7fccd6c8b46
Eder de Bastos <eder.bastos21@gmail.com>
2024-09-10 14:22:16 -0300

    add methods inside inventory class to: hold current weapon and armor; equip new weapons and armors; access current weapon and armor

--- src/main/java/Character/inventory.java
+++ src/main/java/Character/inventory.java
@@ -1,5 +1,33 @@
 package Character;
 
-public class inventory {
+import equipment.armors;
+import equipment.weapons;
 
+public class inventory {
+    weapons heldWeapon;
+    armors heldArmor;
+
+    //holds current weapon and armor
+    public inventory() {
+        this.heldWeapon = weapons.TrainingSword;
+        this.heldArmor = armors.basicClothing;
+    }
+
+    //Methods to equip weapons and armors
+    public void equipWeapon(weapons newWeapon){
+        this.heldWeapon = newWeapon;
+    }
+
+    public void equipArmor (armors newArmor){
+        this.heldArmor = newArmor;
+    }
+
+    //Access current held weapon and armor
+    public weapons getHeldWeapon () {
+        return heldWeapon;
+    }
+
+    public armors getHeldArmor () {
+        return heldArmor;
+    }
 }
