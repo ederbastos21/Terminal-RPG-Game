68df117dc19e29416c9ae151d605fa729ea5b4f1
Eder de Bastos <eder.bastos21@gmail.com>
2024-09-10 01:04:01 -0300

    solve this.name inversion and add public static on status class

--- src/main/java/Character/status.java
+++ src/main/java/Character/status.java
@@ -11,16 +11,17 @@ public class status {
     int charisma;
     int experience;
 
-    public status (String name, int health, int strength, int dexterity, int constitution, int intelligence, int charisma, int experience){
-        name = this.name;
-        health = this.health;
-        strength = this.strength;
-        dexterity = this.dexterity;
-        constitution = this.constitution;
-        intelligence = this.intelligence;
-        charisma = this.charisma;
-        experience = this.experience;
+    public status(String name, int health, int strength, int dexterity, int constitution, int intelligence, int charisma, int experience) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.constitution = constitution;
+        this.intelligence = intelligence;
+        this.charisma = charisma;
+        this.experience = experience;
     }
 
-    status basicCharacter = new status ("name", 10, 1, 1, 1, 1, 1, 1);
+
+    public static status basicCharacter = new status ("name", 10, 1, 1, 1, 1, 1, 1);
 }
