7a437d2053588f56bcc45342030782d4ae4eb587
Eder de Bastos <eder.bastos21@gmail.com>
2024-09-11 22:10:38 -0300

    provide basic game structure, with character name selection, as well as a /command command, which will show all commands available

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -11,22 +11,36 @@ public class Main {
         //Create scanner
         Scanner scanner = new Scanner (System.in);
 
-        //Create object
-        character myCharacter = new character("name", 1, 1, 1, 1, 1, 1, 1);
+        //Create objects
+        character myCharacter = new character("", 1, 1, 1, 1, 1, 1, 1);
 
         //Declare variables
         String characterName;
         String action;
 
-        //Obtain Character name
-        System.out.print("What's the name of your character? ");
-        characterName = scanner.next();
-        System.out.print("Type status to check your character's status: ");
-        action = scanner.next();
-
-        if (action.equals("status")){
-            myCharacter.showStatus();
+        //First Message the player gets
+        System.out.print("Welcome to the game. Please type the name of your character: ");
+        while (true) {
+            characterName = scanner.next();
+            if (characterName.matches("[a-zA-Z]+")){
+                myCharacter.setName(characterName);
+                break;
+            } else {
+                System.out.print("Please place a valid character name (text only): ");
+            }
         }
-        System.out.println(myCharacter.dealDamage());
+
+        System.out.println("At any point, if you want to check the commands, type /commands.");
+
+        while (true) {
+            action = scanner.next();
+            if (action.equalsIgnoreCase("/commands")) {
+                System.out.printf("LIST OF COMMANDS: \n status - Shows character's status \n");
+            } else if (action.equalsIgnoreCase("status")){
+                myCharacter.showStatus();
+            } else{
+                System.out.println("Command not recognized. Type /commands to check all available commands");
+            }
+        }
     }
 }
